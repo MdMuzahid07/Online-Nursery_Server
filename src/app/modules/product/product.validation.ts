61ee0ddd36b4827d4f8c92160bb64fc8d36dca07
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-08-21 13:05:44 +0600

    updated

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -1,1 +1,12 @@
+import { z } from "zod";
 
+
+
+export const productValidationSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    price: z.string(),
+    quantity: z.string(),
+    rating: z.string(),
+    category: z.string()
+});
