9939a79dff46aada673eae00c94eb0d2e51d238f
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-08-22 20:12:57 +0600

    product filtering, searching

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -4,6 +4,9 @@ import { TProduct } from "./product.interface";
 import ProductModel from "./product.model";
 
 
+
+
+
 const addProductIntoDB = async (file: any, payload: TProduct) => {
 
     // create an product object
@@ -32,33 +35,77 @@ const addProductIntoDB = async (file: any, payload: TProduct) => {
     return result;
 };
 
+
+
+
+
+
+
 const getProductsFromDB = async (query: Record<string, unknown>) => {
 
-    console.log(query)
+    //! searching ======> starts here <=======
+    // search will operate in title, or category,
+    // and to make it partial searchable, using $regex here
+    // and for DRY code , using map() here, instead of two time of $regex operation
+    // { "title": { $reges: searchTerm, $options: "i" } }
+
     let searchTerm = " ";
     if (query?.searchTerm) {
         searchTerm = query?.searchTerm as string;
     };
+    // product searchable fields
+    const productSearchableFields = ["title", "category"];
 
-    const searchQuery = ProductModel.find({
-        // search will operate in title, or category, 
-        // and to make it partial searchable, using $regex here
-        // and for DRY code , using map() here, instead of two time of $regex operation
-        $or: ["title", "category"].map((field) => (
-            // { "title": { $reges: searchTerm, $options: "i" } }
-            { [field]: { $regex: searchTerm, $options: "i" } }
-        ))
-    });
+    const searchQuery = ProductModel.find(
+        {
+            $or: productSearchableFields.map((field) => (
+                { [field]: { $regex: searchTerm, $options: "i" } }
+            ))
+        }
+    );
 
-    const result = await searchQuery.find(query);
+    //! searching ======> end here <=======
+
+
+
+
+    //! filtering ======> starts here <=======
+
+    // exclude fields from query those we don't want for filtering
+    //  main query includes with searchTerm, which for partial match search only
+    //  when we trying to filter using main query its include searchTerm as an exact match
+    //  thats why, making an copy of main query, and excluding those fields which not for exact match
+    //  and using the copy version of query for filtering
+
+    const queryObj = { ...query };
+
+    const excludeFieldsForFiltering = ["searchTerm"];
+    excludeFieldsForFiltering.forEach((el) => delete queryObj[el]);
+
+    //! filtering ======> end here <=======
+
+
+    const result = await searchQuery.find(queryObj);
 
     return result;
 };
+
+
+
+
+
+
 
 const getAProductFromDB = async (id: string) => {
     const result = await ProductModel.findById({ _id: id });
     return result;
 };
+
+
+
+
+
+
 
 const updateAProductFromDB = async (id: string, payload: Partial<TProduct>) => {
     const result = await ProductModel.findOneAndUpdate({ _id: id }, payload, {
@@ -68,10 +115,21 @@ const updateAProductFromDB = async (id: string, payload: Partial<TProduct>) => {
     return result;
 };
 
+
+
+
+
+
 const deleteAProductFromDB = async (id: string) => {
     const result = await ProductModel.findOneAndDelete({ _id: id });
     return result;
 };
+
+
+
+
+
+
 
 
 export const ProductService = {
